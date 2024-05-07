6fd32f482547a46ce37a7dea6a0b4e3ab3ac8554
XraySierra0211 <takigawamikoto@gmail.com>
2024-05-07 22:12:05 +0900

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,31 +11,8 @@ If you have any questions, checkout our [documentation](https://docs.streamlit.i
 forums](https://discuss.streamlit.io).
 
 In the meantime, below is an example of what you can do with just a few lines of code:
-hello world
 """
 
-num_points = st.slider("Number of points in spiral", 1, 12000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+data = pd.read_csv('https://mikoweb.net/db/2023.csv')
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
-
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
-
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+st.write(data)
