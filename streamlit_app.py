dc782143c601da6b44f6685a13d7a97e8aa5f8a3
XraySierra0211 <takigawamikoto@gmail.com>
2024-05-12 18:09:28 +0900

    積み上げ棒グラフ作成

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,16 +3,25 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
+
+# C:\Users\takig\AppData\Roaming\Python\Python311\Scripts\streamlit run streamlit_app.py
+# これでローカルホスト
 """
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
+# カテゴリーごとの積み上げ棒グラフ
 """
 
-data = pd.read_csv('https://mikoweb.net/db/2023.csv')
+data = pd.read_csv('shunyu_shishutu.csv')
+# st.write(data)
 
-st.write(data)
+data_1 = data.groupby(['created_at', 'category_code'])['amount'].sum().reset_index()
+# st.write(data_1)
+# st.write(type(data_1))
+
+pivot_df = data_1.pivot(index='created_at', columns='category_code', values='amount').reset_index()
+pivot_df['created_at'] = pd.to_datetime(pivot_df['created_at'], format='%Y/%m/%d')
+pivot_df = pivot_df.sort_values('created_at')
+st.write(pivot_df)
+
+st.bar_chart(
+    pivot_df, x='created_at', y=["Z", "B","C","D","E"]#, color=[]
+)
